0b13798c2861487c89a0e6717923546196748891
darhyl borcelis <133373572+henumberone@users.noreply.github.com>
2025-09-15 00:21:59 +0100

    new change

--- Gumanakana.py
+++ Gumanakana.py
@@ -5,10 +5,11 @@ pygame.init()
 
 # Window
 SCREEN = pygame.display.set_mode((1000, 600))
-pygame.display.set_caption("Any Game")
+pygame.display.set_caption("Text Twist")
 
 # Fonts
-FONT = pygame.font.Font("assets/Font/Hevilla.ttf", 22)
+BIG_FONT = pygame.font.Font("assets/font/Hevilla.ttf", 70)
+FONT = pygame.font.Font("assets/font/Hevilla.ttf", 22)
 
 # Color
 WHITE = (255, 255, 255)
@@ -23,14 +24,16 @@ BORD_IMAGE = "assets/button/bord.png"
 PLACEHOLDER_IMAGE = "assets/button/ans.png"
 BUTTON_IMAGE = "assets/button/button.png"
 LIFE_IMAGE = "assets/button/life.png"
-BANER_IMAGE = "assets/button/baner.png"
+BANER_IMAGE = "assets/button/banner.png"
+HEART = "assets/button/heart.png"
 
-# Backround image
+# Background image
 background = pygame.image.load(BACKGROUND_IMAGE)
 background = pygame.transform.scale(background, (1000, 600))
 
+
 # Dictionary
-FOODS = {
+FOOD = {
     1: ["CAT", "DOG", "COW", "PIG", "BAT", "RAT", "FOX", "HEN", "ANT", "OWL"],
     2: ["LION", "BEAR", "WOLF", "FROG", "DEER", "DUCK", "GOAT", "CRAB", "SWAN", "TOAD"],
     3: ["HORSE", "SHEEP", "MOUSE", "TIGER", "ZEBRA", "PANDA", "SNAKE", "LLAMA", "EAGLE", "SHARK"],
@@ -43,9 +46,10 @@ FOODS = {
 }
 
 # Database
-category = FOODS
+category = FOOD
 game_state = "Level"
 current_level = 1
+attempt = 0
 letters = []
 choices = []
 guests = []
@@ -53,16 +57,16 @@ player_ans = []
 
 
 class Box:  # Create box
-    def __init__(self, x_cord, y_cord, width, height, name, image, font):
+    def __init__(self, x_cord, y_cord, width, height, name, image, font, color):
         self.rect = pygame.Rect(x_cord, y_cord, width, height)
         self.letter = name
         self.font = font
         self.active = True
         self.image = pygame.transform.scale(
             pygame.image.load(image), (width, height))
-        self.name = font.render(name, True, BLACK)
+        self.name = font.render(name, True, color)
 
-    def update_text(self, name=None, visible=True, color=BLACK):
+    def update_text(self, name=None, visible=True, color=WHITE):
         if name is not None:
             self.letter = name
 
@@ -77,6 +81,19 @@ class Box:  # Create box
         surface.blit(self.image, self.rect)
         text_rect = self.name.get_rect(center=self.rect.center)
         surface.blit(self.name, text_rect)
+
+    def draw_inner_image(self, surface, image, width, height, offset_x=0, offset_y=0, active=True):
+        if not active:
+            return None
+
+        inner_image = pygame.image.load(image)
+        inner_image = pygame.transform.scale(inner_image, (width, height))
+
+        inner_rect = inner_image.get_rect()
+        inner_rect.x = self.rect.x + offset_x
+        inner_rect.y = self.rect.y + offset_y
+
+        surface.blit(inner_image, inner_rect)
 
 
 def rand_Level_Words(Level_Words, player_ans):
@@ -93,17 +110,18 @@ def rand_Level_Words(Level_Words, player_ans):
 
 def choices_box(shuffled_letters):  # Create choices box
     rect = []
-    start_x = (1000 - (len(shuffled_letters) * 90)) // 2
+    x_cord = (1000 - (len(shuffled_letters) * 90)) // 2
     y_cord = 470
     for i, char in enumerate(shuffled_letters):
         now = Box(
-            x_cord=start_x + i * 90,
+            x_cord=x_cord + i * 90,
             y_cord=y_cord,
             width=80,
             height=50,
             name=char,
             image=CHOICES_IMAGE,
-            font=FONT
+            font=FONT,
+            color=WHITE
         )
         rect.append(now)
     return rect
@@ -117,23 +135,29 @@ def choices_box(shuffled_letters):  # Create choices box
 def button_box():
     ans = "".join(guests)
 
-    black_bord = Box(200, 60, 600, 300, " ", BORD_IMAGE, FONT)
-    ans_placeholder = Box(250, 400, 500, 50, ans, PLACEHOLDER_IMAGE, FONT)
-    life = Box(830, 30, 140, 50, "", LIFE_IMAGE, FONT)
-    baner = Box(0, 30, 200, 220, "", BANER_IMAGE, FONT)
+    black_bord = Box(150, -190, 700, 800, " ", BORD_IMAGE, FONT, BLACK)
+    ans_placeholder = Box(250, 400, 500, 50, ans,
+                          PLACEHOLDER_IMAGE, FONT, WHITE)
+    life = Box(830, 5, 140, 120, "", LIFE_IMAGE, FONT, WHITE)
+    baner = Box(50, 30, 130, 210, "", BANER_IMAGE, FONT, WHITE)
 
-    enter = Box(850, 100, 100, 50, "Enter", BUTTON_IMAGE, FONT)
-    delete = Box(850, 170, 100, 50, "Delete", BUTTON_IMAGE, FONT)
-    shuffle = Box(850, 240, 100, 50, "Shuffle", BUTTON_IMAGE, FONT)
+    enter = Box(690, -15, 400, 350, "Enter", BUTTON_IMAGE, FONT, WHITE)
+    delete = Box(690, 35, 400, 350, "Delete", BUTTON_IMAGE, FONT, WHITE)
+    shuffle = Box(690, 85, 400, 350, "Shuffle", BUTTON_IMAGE, FONT, WHITE)
 
-    # Draw Design
+    # Draw design
     black_bord.draw(SCREEN)
     life.draw(SCREEN)
     baner.draw(SCREEN)
-
-    # Update answer placeholder before drawing
-    ans_placeholder.update_text(name=ans, color=WHITE)
     ans_placeholder.draw(SCREEN)
+
+    # life
+    attempt_left = 4
+    remaining_lives = attempt_left - attempt
+    heart_positions = [(25, 40), (50, 35), (75, 35), (100, 40)]
+    for num in range(remaining_lives):
+        offset_x, offset_y = heart_positions[num]
+        life.draw_inner_image(SCREEN, HEART, 20, 20, offset_x, offset_y, True)
 
     # Draw buttons
     enter.draw(SCREEN)
@@ -141,6 +165,12 @@ def button_box():
     shuffle.draw(SCREEN)
 
     return enter, delete, shuffle
+
+
+def text(name, color=WHITE, x_cord=0, y_cord=0, font=BIG_FONT):
+    txt = font.render(name, True, color)
+    title = SCREEN.blit(txt, (x_cord, y_cord))
+    return title
 
 
 def draw_game(choices):  # Draw game
@@ -157,23 +187,25 @@ def draw_game(choices):  # Draw game
 #                   name="Level", image=LEVEL_IMAGE, font=FONT)
 
 # Variables
-Level_Words = FOODS[current_level]  # all the word in level
+Level_Words = category[current_level]  # all the word in level
 shuffled_letters = rand_Level_Words(
     Level_Words, player_ans)  # shuffled_letters
 choices = choices_box(shuffled_letters)  # put the shuffled_letters in box
 
 # Button
-play = Box(x_cord=120, y_cord=280, width=80, height=50,
-           name="play", image=PLAY_IMAGE, font=FONT)
+play = Box(x_cord=430, y_cord=280, width=120, height=60,
+           name=" ", image=PLAY_IMAGE, font=FONT, color=BLACK)
+
 
 # Game loop
 run = True
 while run:
-    SCREEN.blit(background, (0, 0))
+    SCREEN.blit(background, (0, 0))  # Draw background
 
     # Game state : Menu
     if game_state == "Menu":
         play.draw(SCREEN)
+        title = text("TEXT TWIST", WHITE, 310, 50, BIG_FONT)
 
     # Game state : Play
     elif game_state == "Play":
@@ -215,7 +247,6 @@ while run:
                     game_state = "Level"
 
         elif game_state == "Level":
-
             # Mouse
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = event.pos
@@ -242,9 +273,15 @@ while run:
                             choices = choices_box(shuffled_letters)
                         else:
                             print("You win")
-                    else:
+
+                    elif now not in player_ans:
+                        print(player_ans)
+                        attempt += 1
                         for box in choices:
                             box.active = True
+
+                    if attempt == 4:
+                        print("You Loose")
 
             # Keyboard
             if event.type == pygame.KEYDOWN:
@@ -253,6 +290,7 @@ while run:
                     for box in choices:
                         box.active = True
                     game_state = "Menu"
+                    attempt = 0
 
                 elif event.key == pygame.K_SPACE:
                     guests = []
@@ -270,6 +308,7 @@ while run:
                 elif event.key == pygame.K_RETURN:
                     now = "".join(guests).upper()
                     guests = []
+
                     if now in Level_Words and now not in player_ans:
                         player_ans.append(now)
                         shuffled_letters = rand_Level_Words(
@@ -278,9 +317,14 @@ while run:
                             choices = choices_box(shuffled_letters)
                         else:
                             print("You win")
-                    else:
+
+                    elif now not in player_ans:
+                        attempt += 1
                         for box in choices:
                             box.active = True
+
+                    if attempt == 4:
+                        print("You loose")
 
                 else:
                     typed_char = event.unicode.upper()
